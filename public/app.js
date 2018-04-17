b188e08868cf9f5720aa248d01a0800469158a15
Alex Fedenczuk <alex.fedenczuk@gmail.com>
2018-04-17 09:31:42 +0100

    Tidied up after review

--- public/app.js
+++ public/app.js
@@ -1,25 +1,17 @@
+const Cat = function (name, favouriteFood, imageURL) {
+  this.name = name;
+  this.favouriteFood = favouriteFood;
+  this.imageURL = imageURL;
+};
+
 document.addEventListener('DOMContentLoaded', () => {
 
-  catsCollection = [{
-    name: 'Boba',
-    favouriteFood: 'Sock fluff',
-    image: 'http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg'
-  },
-  {
-    name: 'Barnaby',
-    favouriteFood: 'Tuna',
-    image: 'https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg'
-  },
-  {
-    name: 'Max',
-    favouriteFood: 'Whiskas Temptations',
-    image: 'http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg'
-  },
-  {
-    name: 'Derp',
-    favouriteFood: 'Cheese',
-    image: 'images/cat.jpg'
-  }];
+  catsCollection = [
+    new Cat('Boba', 'Sock Fluff', 'http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg'),
+    new Cat('Barnaby', 'Tuna', 'https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg'),
+    new Cat('Max', 'Whiskas Temptations', 'http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg'),
+    new Cat('Derp', 'Cheese', 'images/cat.jpg')
+  ];
 
   const cats = document.querySelector('#cats');
   cats.innerHTML = '';
@@ -28,23 +20,19 @@ document.addEventListener('DOMContentLoaded', () => {
     return document.createElement(element);
   }
 
-  const addCat = function (catData, container) {
-
-    const name = catData.name;
-    const favouriteFood = catData.favouriteFood;
-    const image = catData.image;
+  const addCat = function (cat, container) {
 
     const catInfo = elementCreator('ul');
 
     const catName = elementCreator('li');
-    catName.textContent = name;
+    catName.textContent = cat.name;
 
     const catFavFood = elementCreator('li');
-    catFavFood.textContent =favouriteFood;
+    catFavFood.textContent =cat.favouriteFood;
 
-    const catImageLI= elementCreator('li');
+    const catImageLI = elementCreator('li');
     const catImage = elementCreator('img');
-    catImage.src = image;
+    catImage.src = cat.imageURL;
 
     cats.appendChild(catInfo);
 
